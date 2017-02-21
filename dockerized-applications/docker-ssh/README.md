62dd464ded591e3102b601f2916fb440a2c21380
Vish <savishy.pv@gmail.com>
2017-02-21 12:13:57 +0000

    updated script to make it work for centos

--- dockerized-applications/docker-ssh/README.md
+++ dockerized-applications/docker-ssh/README.md
@@ -1,1 +1,17 @@
-work in progress: Docker container that you can SSH into.
+*This is a work in progress.*
+This is a Centos 6 based Docker container that you can SSH into.
+
+## How to run
+
+Execute the `create.sh` script. This does the following
+* create key pair `ansible_id_rsa` and `ansible_id_rsa.pub`.
+* Paste a local copy (in current working dir) which will be used for Docker image creation.
+* Paste a copy to `$HOME/.ssh/`.
+* Create a Docker Image `docker-ssh` which contains centos 6 *plus all customizations required for SSH into a CentOS container*.
+* Run a container off it, `docker-ssh-container`.
+
+Now, to SSH to it, Execute
+
+```
+ssh -i ~/.ssh/ansible_id_rsa root@172.17.0.2 -p 22
+```
--- dockerized-applications/docker-ssh/create.sh
+++ dockerized-applications/docker-ssh/create.sh
@@ -2,7 +2,17 @@
 set -e
 # do not prompt http://stackoverflow.com/a/15890748/682912
 echo "-- creating an SSH key without passphrase (NOT FOR PRODUCTION)"
-echo -e  'n\n' | ssh-keygen -b 2048 -t rsa -f ./ansible_id_rsa -q -N ""
-chmod 600 ansible_id_rsa
+if [ ! -e ~/.ssh/ansible_id_rsa ]; then
+  ssh-keygen -b 2048 -t rsa -f ~/.ssh/ansible_id_rsa -q -N ""
+fi
+cp -v ~/.ssh/ansible_id_rsa* .
 echo "-- creating Docker Image"
 docker build -t docker-ssh .
+chmod 600 ~/.ssh/ansible_id_rsa
+chmod 600 ~/.ssh/ansible_id_rsa.pub
+
+echo "-- removing any old entries from known_hosts"
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R 172.17.0.2
+
+echo "-- running Docker container"
+docker run -d -p 2200:22 --name docker-ssh-container docker-ssh
