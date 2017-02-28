c2d3034ad0d3879f77d11d56c7ba61471fa361f5
Vish <savishy.pv@gmail.com>
2017-02-28 06:57:47 +0000

    cosmetic changes, added an optional argument to enable first-time key creation

--- dockerized-applications/docker-ssh/create.sh
+++ dockerized-applications/docker-ssh/create.sh
@@ -11,21 +11,27 @@ if [[ ! "$prevContainers" == "" ]]; then
   echo $prevContainers | xargs docker rm -f
 fi
 
-echo "-- creating SSH key $KEY_PATH  (Warning: this step may fail during repeated runs -- WIP)"
-if [ ! -e $KEY_PATH ]; then
-  ssh-keygen -b 2048 -t rsa -f $KEY_PATH -q -N ""
+if [[ "$1" == "true" ]]; then
+  echo "-- creating SSH key $KEY_PATH  (Warning: this step may fail during repeated runs -- WIP)"
+  if [ ! -e $KEY_PATH ]; then
+    ssh-keygen -b 2048 -t rsa -f $KEY_PATH -q -N ""
+  else
+    echo "-- key exists at $KEY_PATH; skipping"
+  fi
+  cp -v $KEY_PATH .
+  cp -v $KEY_PATH.pub .
+  chmod 600 $KEY_PATH
+  chmod 600 $KEY_PATH.pub
+else
+  echo "-- skipping SSH key creation at $KEY_PATH (run the script with $0 true to enable this)"
 fi
 
-cp -v $KEY_PATH .
-cp -v $KEY_PATH.pub .
 echo "-- creating Docker Image"
 docker build -t $IMG_NAME .
-chmod 600 $KEY_PATH
-chmod 600 $KEY_PATH.pub
 
 echo "-- running Docker container"
 docker run -d -p 2200:22 --name $CONTAINER_NAME $IMG_NAME
-
+CONTAINER_ID=`docker ps -aq -f "name=$CONTAINER_NAME"`
 ipAddr=`docker inspect -f {{.NetworkSettings.IPAddress}} $CONTAINER_NAME`
 echo "-- container IP: $ipAddr"
 echo "-- removing any old entries from known_hosts"
@@ -35,3 +41,10 @@ echo "-- creating inventory file (for use by Ansible)"
 commonArgs="ansible_ssh_port=22 ansible_ssh_user='root' ansible_ssh_private_key_file=$KEY_PATH"
 inventoryLine="$CONTAINER_NAME ansible_ssh_host=$ipAddr $commonArgs"
 echo "$inventoryLine" > inventory
+echo "--------------------------"
+echo "-- inventory file created at ./inventory"
+echo "-- docker container created with name $CONTAINER_NAME"
+echo "   and ID: $CONTAINER_ID"
+echo "-- To SSH into this container, do the following: "
+echo "   ssh -i $KEY_PATH root@$ipAddr"
+echo "--------------------------"
