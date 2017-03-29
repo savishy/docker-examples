f7a516f64563e93a3e99dbb7ed09832c7c4f8ddc
Vish <savishy.pv@gmail.com>
2017-03-29 07:53:12 +0000

    fix for timeout when running Jenkins CLI command https://issues.jenkins-ci.org/browse/JENKINS-38623

--- dockerized-applications/docker-jenkins/run.sh
+++ dockerized-applications/docker-jenkins/run.sh
@@ -1,2 +1,2 @@
 docker stop jenkins-container && docker rm jenkins-container
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/jenkins-home:/var/lib/jenkins -p 8081:8081 --name jenkins-container docker-jenkins
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/jenkins-home:/var/lib/jenkins -p 8081:8081 --name jenkins-container docker-jenkins
