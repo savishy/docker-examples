c59411c94740cef2ef1d41bfc67c92a55fed9fdb
Vish <savishy.pv@gmail.com>
2017-04-19 09:17:47 +0000

    docker swarm: WIP

--- docker-swarm/docker-swarm-mode-1.12/1-manual/createSwarm.sh
+++ docker-swarm/docker-swarm-mode-1.12/1-manual/createSwarm.sh
@@ -7,6 +7,11 @@ log() {
   printf "${GREEN}--- $1${NC}\n"
 }
 
+logError() {
+  RED='\033[0;31m'
+  NC='\033[0m' # No Color
+  printf "${RED}--- $1${NC}\n"
+}
 
 log "--------------------------------"
 log "SWARM CREATION SCRIPT"
@@ -23,7 +28,7 @@ else
 fi
 
 doAWSChecks() {
-  echo "-- initial checks..."
+  log "-- initial checks..."
   errors=false
   for var in  AWS_ACCESS_KEY \
               AWS_SECRET_KEY \
@@ -34,12 +39,12 @@ doAWSChecks() {
               AWS_DEFAULT_ZONE \
               AWS_SECURITYGROUP; do
     if [[ "${!var}" == "" ]]; then
-      echo "ERROR: $var is not set! Did you set the variable in setenv.sh?"
+      logError "ERROR: $var is not set! Did you set the variable in setenv.sh?"
       errors=true
     fi
   done
   if $errors; then
-    echo "Please fix the above errors and try again" \
+    logError "Please fix the above errors and try again" \
     && exit 1
   fi
 }
@@ -57,26 +62,23 @@ if [[ "$DRIVER" == "amazonec2" ]]; then
 elif [[ "$DRIVER" == "virtualbox" ]]; then
     COMMON_OPTS=""
 else
-  echo "unsupported driver: $DRIVER" && exit 1
+  logError "unsupported driver: $DRIVER" && exit 1
 fi
 
-echo "---- using driver $DRIVER"
-# remove previous machines.
-echo "---- removing any previous docker-machines"
-docker-machine rm -f manager0
-docker-machine rm -f worker0
-docker-machine rm -f worker1
+log "---- using driver $DRIVER"
 
-echo "---- creating docker machines manager0, worker0, worker1"
-docker-machine create -d $DRIVER $COMMON_OPTS manager0
-docker-machine create -d $DRIVER $COMMON_OPTS worker0
-docker-machine create -d $DRIVER $COMMON_OPTS worker1
-
+for machineName in manager0 worker0 worker1; do
+  # remove previous machines.
+  log "---- removing previous docker-machine $machineName"
+  docker-machine rm -f $machineName
+  log "---- creating docker machine $machineName"
+  docker-machine create -d $DRIVER $COMMON_OPTS $machineName
+done
 
 eval $(docker-machine env manager0)
 # get ip of manager0
 MANAGER0_IP=$(docker-machine ip manager0)
-echo "---- creating swarm manager; machine ip $MANAGER0_IP"
+log "---- creating swarm manager; machine ip $MANAGER0_IP"
 docker swarm init --advertise-addr $MANAGER0_IP
 
 # get manager and worker tokens
@@ -85,7 +87,7 @@ WORKER_TOKEN=$(docker swarm join-token -q worker)
 
 WORKER_JOIN_CMD="docker swarm join --token $WORKER_TOKEN $MANAGER0_IP:2377"
 
-echo "---- joining worker0, worker1 to swarm"
+log "---- joining worker0, worker1 to swarm"
 eval $(docker-machine env worker0)
 eval $WORKER_JOIN_CMD
 eval $(docker-machine env worker1)
